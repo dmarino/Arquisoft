7616ae97002e1fa6edee57458c4d6d5b3d4febc8
JmLizarazo10 <jm.lizarazo10@uniandes.edu.co>
2017-04-29 19:04:16 -0500

    subiendo config loaderIo

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import aws.SQSConnection;
+import com.typesafe.config.ConfigFactory;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
@@ -10,6 +13,20 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
+    public static final String AWS_S3_BUCKET = "aws.s3.bucket";
+    public static final String AWS_ACCESS_KEY = "aws.access.key";
+    public static final String AWS_SECRET_KEY = "aws.secret.key";
+
+    public Result loaderio() {
+        return ok("loaderio-c2b1b157954d782cf1629a87e9aa0e01");
+    }
+
+    public Result enviar(){
+        SQSConnection sqs = new SQSConnection();
+        sqs.sendMessage("Hola");
+        return ok("Enviando");
+    }
+
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
@@ -17,8 +34,8 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(home.render());
+        return ok(index.render(""));
     }
-    //public Result index() {return ok(index.render("hospital santa fe"));}
+
 
 }
