79288374d6746d2c48e50d177ca50978ac149d4b
JmLizarazo10 <jm.lizarazo10@uniandes.edu.co>
2017-05-10 11:59:34 -0500

    cambios

--- app/aws/SQSConnection.java
+++ app/aws/SQSConnection.java
@@ -35,8 +35,8 @@ public class SQSConnection {
     }
 
     public void onStart() {
-        String accessKey = ConfigFactory.load().getString(AWS_ACCESS_KEY);
-        String secretKey = ConfigFactory.load().getString(AWS_SECRET_KEY);
+        String accessKey = "AKIAJ7VUOPUHMWEB3THQ";
+        String secretKey = "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";
 
         if ((accessKey != null) && (secretKey != null)) {
             AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
